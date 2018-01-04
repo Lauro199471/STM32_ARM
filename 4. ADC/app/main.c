afcad185d64d859e6b90c9736513bc5418f2aaa7
Lauro Cabral <Lauro199471@gmail.com>
2018-01-03 20:56:04 -0800

    Added LCD fuctions for viewing of ADC

--- 4. ADC/app/main.c
+++ 4. ADC/app/main.c
@@ -1,26 +1,14 @@
-//*****************************************************************************
-//
-//! \file main.c
-//! \brief main application
-//! \version 1.0.0.0
-//! \date $Creat_time$
-//! \author $Creat_author$
-//! \copy
-//!
-//! Copyright (c) 2014 CooCox.  All rights reserved.
-//
-//! \addtogroup project
-//! @{
-//! \addtogroup main
-//! @{
-//*****************************************************************************
+#include "stm32f0xx.h"
+#include "port_initialize.h"
+#include "LCDfunctions.h"
 
 int main(void)
 {
-
-    while(1)
-    {
-    }
+	PortA_Init();
+  PortB_Init();
+  PortC_Init();
+  Init_lcd();
+  while(1)
+  {
+  }
 }
-//! @}
-//! @}
